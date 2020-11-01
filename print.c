b5078be7a3c80b5d3b8e5222a65040a18f86cc0a
Sibonji <70277546+Sibonji@users.noreply.github.com>
2020-11-01 10:09:59 +0300

    Create print.c

---
+++ print.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "head_bank.h"
+
+void print(struct balance *total_balance, struct currency *val)
+    {
+    int i = 0;
+    int j = 0;
+
+    for(i = 0; i < 5; i++)
+	{
+	printf("Name: %s,  code: %s, total balance: %d\n", val[i].currency_name, val[i].letter_code, total_balance->currency_start[i]);
+	}
+
+    for(i = 0; i < 5; i++)
+        {
+        printf("Name: %s,  code: %s, total balance: %d\n", val[i].currency_name, val[i].letter_code, total_balance->currency_cipher[i]);
+        }
+    }
